fdae4404bb1d7efeab0a764c3298bbcaf0ffc354
Duckduckgame <35844966+Duckduckgame@users.noreply.github.com>
2020-01-31 21:59:53 +0000

    basic navmesh functionality added

---
+++ TowerDefenseCleanUpCrew/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemy;
+
+    //List<GameObject> targets;
+    [SerializeField]
+    GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 0, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*IEnumerator SpawnEnemy()
+    {
+        Instantiate(enemy, transform.position, Quaternion.identity);
+        yield return null;
+    }*/
+
+    void SpawnEnemy()
+    {
+        GameObject enemyGO = Instantiate(enemy, transform.position, Quaternion.identity);
+        enemyGO.GetComponent<EnemyUnit>().target = target;
+    }
+}
---
+++ TowerDefenseCleanUpCrew/Assets/Scripts/EnemyUnit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyUnit : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Vector3 pos = target.transform.position;
+        pos += new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2));
+        agent.destination = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
