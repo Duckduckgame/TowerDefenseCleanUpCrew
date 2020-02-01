b8dd1f163cd4941a8f17787810ec24c79c1c3082
Duckduckgame <35844966+Duckduckgame@users.noreply.github.com>
2020-02-01 17:29:08 +0000

    added gate functionality

---
+++ TowerDefenseCleanUpCrew/Assets/Scripts/GateHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateHandler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject gate;
+    FixingHandler fixingHandler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fixingHandler = GetComponent<FixingHandler>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fixingHandler.life <= 0)
+        {
+            gate.SetActive(false);
+            GetComponent<UnityEngine.AI.NavMeshObstacle>().enabled = false;
+        }
+    }
+}
