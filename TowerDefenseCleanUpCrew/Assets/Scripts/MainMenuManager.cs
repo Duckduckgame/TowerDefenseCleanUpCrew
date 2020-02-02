bb871df67dcee3a4d76b02e5eb3ea53e8ac1ed7a
Duckduckgame <35844966+Duckduckgame@users.noreply.github.com>
2020-02-02 14:02:40 +0000

    main menu added

---
+++ TowerDefenseCleanUpCrew/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    public void StartLevelOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartLevelTwo()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Tutorial()
+    {
+
+    }
+}
